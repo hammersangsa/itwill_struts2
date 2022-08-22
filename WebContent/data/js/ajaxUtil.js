8dc08b25779a9174ff1565428961384bca949a03
HeeSeong Eom <gmltjd9801@naver.com>
2022-08-22 18:33:59 +0900

    util

--- WebContent/data/js/ajaxUtil.js
+++ WebContent/data/js/ajaxUtil.js
@@ -1,7 +1,57 @@
 
-var xmlHttp;
+var httpRequest;
 
-function createXMLHttpRequest() {
+function getXMLHttpRequest() {
+
+
+    if (window.XMLHttpRequest) {  
+        return new XMLHttpRequest();
+        
+    } else if (window.ActiveXObject) {
+		try {
+			
+			return new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e1) {
+			try {
+				
+				return new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e2) {
+
+			}
+		}
+	}
+
+}
+
+var httpRequest = null;
+//4개의 데이터를 던지면 getXML거쳐서 httpRequest생성
+function sendRequest(url,params,callback,method) {
+	
+	httpRequest = getXMLHttpRequest();
+	//method 처리
+	var httpMethod = method ? method : "GET";//기본값
+	
+	if(httpMethod!="GET" && httpMethod!="POST"){
+		httpMethod = "GET";
+	}
+	//param처리
+	var httpParams = (params==null || params=="") ? null : params;
+	//url처리
+	var httpUrl = url;
+	
+	if(httpMethod=="GET"&&httpParams!=null){
+		httpUrl = httpUrl + "?" + httpParams;
+	}
+	//POST방식
+	httpRequest.open(httpMethod,httpUrl,true);
+	httpRequest.setRequestHeader(
+			"Content-Type", "application/x-www-form-urlencoded");
+	httpRequest.onreadystatechange = callback;
+	httpRequest.send(httpMethod=="POST"?httpParams:null);
+}
+
+
+/*function createXMLHttpRequest() {
     var xmlReq = false;
 
     if (window.XMLHttpRequest) {    // Non-Microsoft browsers
@@ -22,3 +72,4 @@ function createXMLHttpRequest() {
 
 	return xmlReq;
 }
+*/
